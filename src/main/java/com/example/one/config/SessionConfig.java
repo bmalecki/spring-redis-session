6298118a88bf0e3556fd427e10f31a629d5deb37
Bartlomiej Malecki <bartlomiej.malecki@cognifide.com>
2020-11-30 17:50:58 +0100

    enable ssl

--- src/main/java/com/example/one/config/SessionConfig.java
+++ src/main/java/com/example/one/config/SessionConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -32,11 +33,13 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(host);
-        configuration.setPassword(password);
-        configuration.setPort(6379);
+        RedisStandaloneConfiguration defaultRedisConfig = new RedisStandaloneConfiguration();
+        defaultRedisConfig.setHostName(host);
+        defaultRedisConfig.setPassword(password);
+        defaultRedisConfig.setPort(6380);
 
-        return new LettuceConnectionFactory(configuration);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().useSsl().build();
+
+        return new LettuceConnectionFactory(defaultRedisConfig, clientConfig);
     }
 }
